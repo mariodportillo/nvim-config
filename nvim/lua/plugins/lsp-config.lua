8164d353149fe50bfafab3850612abe0e89eae22
mariodportillo <mariodportillo@gmail.com>
2026-05-18 19:10:37 -0700

    Added rust lsp

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -6,7 +6,6 @@ return {
 			require("mason").setup()
 		end,
 	},
-
 	-- Mason LSP config (auto-installs LSPs)
 	{
 		"williamboman/mason-lspconfig.nvim",
@@ -21,51 +20,55 @@ return {
 					"html",
 					"cssls",
 					"ts_ls",
-					"jsonls", -- Added CSS, TypeScript, JavaScript, React (via tsserver)
+					"jsonls",
+					"rust_analyzer", -- Added Rust LSP
 				},
 			})
 		end,
 	},
-
 	-- LSP Config
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			local lspconfig = require("lspconfig")
-
 			-- Lua
 			lspconfig.lua_ls.setup({ capabilities = capabilities })
-
 			-- AST Grep
 			lspconfig.ast_grep.setup({ capabilities = capabilities })
-
 			-- CMake
 			lspconfig.cmake.setup({ capabilities = capabilities })
-
 			-- Python
 			lspconfig.pyright.setup({ capabilities = capabilities })
-
 			-- C/C++
 			lspconfig.clangd.setup({ capabilities = capabilities })
-
 			-- HTML
 			lspconfig.html.setup({ capabilities = capabilities })
-
 			-- CSS
 			lspconfig.cssls.setup({ capabilities = capabilities })
-
 			-- TypeScript & JavaScript (includes React support)
 			lspconfig.ts_ls.setup({
 				capabilities = capabilities,
 				filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact" },
 			})
-
 			-- JSON
 			lspconfig.jsonls.setup({ capabilities = capabilities })
-
+			-- Rust
+			lspconfig.rust_analyzer.setup({
+				capabilities = capabilities,
+				settings = {
+					["rust-analyzer"] = {
+						cargo = {
+							allFeatures = true,
+						},
+						checkOnSave = {
+							command = "clippy",
+						},
+					},
+				},
+			})
 			-- Keybindings
-            vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { noremap = true, silent = true })
+			vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { noremap = true, silent = true })
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
